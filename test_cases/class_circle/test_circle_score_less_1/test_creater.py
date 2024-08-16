7e5f9b6482e97653aad929bd23ab94cb73d8d42b
David Niebert <david.niebert@googlemail.com>
2024-08-16 15:17:43 +0200

    Add tests, fix tests

--- test_cases/class_circle/test_circle_score_less_1/test_creater.py
+++ test_cases/class_circle/test_circle_score_less_1/test_creater.py
@@ -6,28 +6,28 @@ from util import json_util
 class Test(TestModel):
     def create(self):
         match_sms_1 = model.SemanticMatch(
-            base_semantic_id='dxvidnrt.com/semanticID/1',
-            match_semantic_id='dxvidnrt.com/semanticID/2',
+            base_semantic_id='sms1/semanticID/1',
+            match_semantic_id='sms1/semanticID/2',
             score=0.9,
-            meta_information={'matchSource': 'Defined by David Niebert'}
+            meta_information={'matchSource': '1'}
         )
         match_sms_2 = model.SemanticMatch(
-            base_semantic_id='dxvidnrt.com/semanticID/2',
-            match_semantic_id='dxvidnrt.com/semanticID/3',
+            base_semantic_id='sms1/semanticID/2',
+            match_semantic_id='sms1/semanticID/3',
             score=0.9,
-            meta_information={'matchSource': 'Defined by David Niebert'}
+            meta_information={'matchSource': '1'}
         )
         match_sms_3 = model.SemanticMatch(
-            base_semantic_id='dxvidnrt.com/semanticID/3',
-            match_semantic_id='dxvidnrt.com/semanticID/1',
+            base_semantic_id='sms1/semanticID/3',
+            match_semantic_id='sms1/semanticID/1',
             score=0.9,
-            meta_information={'matchSource': 'Defined by David Niebert'}
+            meta_information={'matchSource': '1'}
         )
         matches_list = [match_sms_1, match_sms_2, match_sms_3]
         json_util.save_as_json(self.test_json_path, matches_list)
         json_util.save_as_json(self.expected_minimal_matches_path, [match_sms_2])
         self.match_request = service_model.MatchRequest(
-            semantic_id='dxvidnrt.com/semanticID/1',
-            score_limit=0.6,
+            semantic_id='sms1/semanticID/1',
+            score_limit=0.3,
             local_only=True
         )
--- test_cases/class_multiple_edges/test_multiple_edges/test_creater.py
+++ test_cases/class_multiple_edges/test_multiple_edges/test_creater.py
@@ -1,7 +1,7 @@
 from semantic_matcher import service_model, model
 
 from model.Test import TestModel
-from util import json_util
+from util import json_util, graph_util
 
 
 class Test(TestModel):
@@ -12,33 +12,35 @@ class Test(TestModel):
 
     def create(self):
         match_1 = model.SemanticMatch(
-            base_semantic_id='sms1/semanticID/Dog',
-            match_semantic_id='sms2/semanticID/Cat',
+            base_semantic_id='sms1/semanticID/1',
+            match_semantic_id='sms2/semanticID/2',
             score=0.7,
-            meta_information={'matchSource': 'Defined by David Niebert'}
+            meta_information={'matchSource': '1'}
         )
         match_2 = model.SemanticMatch(
-            base_semantic_id='sms1/semanticID/Dog',
-            match_semantic_id='sms3/semanticID/Dog',
+            base_semantic_id='sms1/semanticID/1',
+            match_semantic_id='sms3/semanticID/1',
             score=1,
-            meta_information={'matchSource': 'Defined by David Niebert'}
+            meta_information={'matchSource': '1'}
         )
         match_3 = model.SemanticMatch(
-            base_semantic_id='sms3/semanticID/Dog',
-            match_semantic_id='sms2/semanticID/Cat',
+            base_semantic_id='sms3/semanticID/1',
+            match_semantic_id='sms2/semanticID/2',
             score=0.8,
-            meta_information={'matchSource': 'Defined by David Niebert'}
+            meta_information={'matchSource': '1'}
         )
         match_4 = model.SemanticMatch(
-            base_semantic_id='sms2/semanticID/Cat',
-            match_semantic_id='sms4/semanticID/Bird',
+            base_semantic_id='sms2/semanticID/2',
+            match_semantic_id='sms4/semanticID/3',
             score=0.5,
-            meta_information={'matchSource': 'Defined by David Niebert'}
+            meta_information={'matchSource': '1'}
         )
         matches = [match_1, match_2, match_4, match_3]
+        expected_matches = graph_util.find_reachable_matches('sms1/semanticID/1', matches, 0.0)
         json_util.save_as_json(self.test_json_path, matches)
+        json_util.save_as_json(self.expected_matches_path, expected_matches)
         self.match_request = service_model.MatchRequest(
-            semantic_id='sms1/semanticID/Dog',
+            semantic_id='sms1/semanticID/1',
             score_limit=0,
             local_only=False
         )
